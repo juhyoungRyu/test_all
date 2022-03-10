32e75b2052e193b72e73070bc5a7c2d78123e149
juhyoungryu <thinkoutside2004@gmail.com>
2022-03-10 18:59:28 +0900

    세팅 중

--- login_test/src/App.js
+++ login_test/src/App.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
@@ -13,12 +13,61 @@ const App = () => {
     callApi();
   }, []);
 
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const [inputValue3, setInputValue3] = useState("");
+  const [inputValue4, setInputValue4] = useState("");
+
+  const changeValue1 = (e) => {
+    setInputValue1(e.target.value);
+  };
+
+  const changeValue2 = (e) => {
+    setInputValue2(e.target.value);
+  };
+
+  const changeValue3 = (e) => {
+    setInputValue3(e.target.value);
+  };
+
+  const changeValue4 = (e) => {
+    setInputValue4(e.target.value);
+  };
+
+  const clickBtn = () => {
+    const saveInput = `${inputValue1}, ${inputValue2}, ${inputValue3}, ${inputValue4}`;
+    console.log(saveInput);
+    setInputValue1("");
+    setInputValue2("");
+    setInputValue3("");
+    setInputValue4("");
+  };
+
   return (
     <div className="App">
-      <input placeholder="아이디"></input>
-      <input placeholder="비밀번호"></input>
-      <input placeholder="이메일"></input>
-      <input placeholder="닉네임"></input>
+      <input
+        value={inputValue1}
+        onChange={changeValue1}
+        placeholder="아이디"
+      ></input>
+      <input
+        value={inputValue2}
+        onChange={changeValue2}
+        placeholder="비밀번호"
+      ></input>
+      <input
+        value={inputValue3}
+        onChange={changeValue3}
+        placeholder="이메일"
+      ></input>
+      <input
+        value={inputValue4}
+        onChange={changeValue4}
+        placeholder="닉네임"
+      ></input>
+      <button onClick={clickBtn} className="signUp">
+        회원가입
+      </button>
     </div>
   );
 };
